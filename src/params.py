a58db3fe54d2b23def7e0ed694b653d2cd595a0f
Tanveer <tanveer.karim@cfa.harvard.edu>
2021-06-29 14:55:16 +0000

    global parameter file

---
+++ src/params.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+#plotting params
+fs = 20
+fsize = (10, 7)
+
+#mock definitions
+tol = 0.8
+NMOCKS = 1000
+
+#Fourier Space definitions
+NSIDE = 1024
+LMIN = 100
+LMAX = 3 * NSIDE - 1
+ell = np.arange(0, LMAX, 1)
+ELL = np.logspace(0, np.log10(LMAX), 10)
+
+#cosmology in CLASS formalism
+h = 0.6736
+Omega_c = 0.12/h**2
+Omega_b = 0.02237/h**2
+A_s = 2.083e-09
+n_s = 0.9649
+b1 = 1.75
+
+#fsky
+mask = np.load("../dat/elg_ran1024.npy")
+mask[mask != 0] = 1 #good pixels are 1
+mask = mask.astype("bool")
+fsky = np.sum(mask)/mask.size
