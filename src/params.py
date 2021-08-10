5a8a40a3337f4d90eb977652f35695287d7704c2
Tanveer <tanveer.karim@cfa.harvard.edu>
2021-08-10 13:26:40 +0000

    update parameter values

--- src/params.py
+++ src/params.py
@@ -7,21 +7,14 @@ fsize = (10, 7)
 #mock definitions
 tol = 0.8
 NMOCKS = 1000
+nbar_sqdeg = 2400 #number density of galaxies
 
 #Fourier Space definitions
 NSIDE = 1024
 LMIN = 100
-LMAX = 2 * NSIDE
+LMAX = NSIDE
 ell = np.arange(0, LMAX, 1)
 ELL = np.logspace(0, np.log10(LMAX), 10)
-
-#cosmology in CLASS formalism
-h = 0.6736
-Omega_c = 0.12/h**2
-Omega_b = 0.02237/h**2
-A_s = 2.083e-09
-n_s = 0.9649
-b1 = 1.75
 
 #fsky
 mask = np.load("../dat/elg_ran1024.npy")
